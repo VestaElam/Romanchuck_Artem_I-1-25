6611bf24d62cf2e65d2cd76194f94794e4d55528
VestaElam <magikrush9@gmail.com>
2026-02-17 11:50:24 +0300

    Create АВС_lab1.cpp

---
+++ АВС_lab1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int number;
+    string binaryStr = "";
+
+    cout << "Введите целое число (десятичное): ";
+    cin >> number;
+
+    int temp = number;
+    if (temp == 0) {
+        binaryStr = "0";
+    }
+    else {
+        while (temp > 0) {
+            binaryStr = to_string(temp % 2) + binaryStr;
+            temp = temp / 2;
+        }
+    }
+
+    int countOnes = 0;
+    for (int i = 0; i < binaryStr.length(); i++) {
+        char bit = binaryStr[i];
+        if (bit == '1') {
+            countOnes++;
+        }
+    }
+
+    cout << "Двоичное представление числа " << number << ": " << binaryStr << endl;
+    cout << "Количество единиц в двоичном представлении: " << countOnes << endl;
+
+    return 0;
+}
