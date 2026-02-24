a7a125acb0285d71624f5cbe198931abcdc3c47b
VestaElam <magikrush9@gmail.com>
2026-02-24 10:40:57 +0300

    Create ABC_lab№2.cpp

---
+++ ABC_lab№2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const int size = 10;
+    float numbers[size];
+
+    cout << "Введите " << size << " чисел для массива:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "numbers[" << i << "] = ";
+        cin >> numbers[i];
+    }
+
+    float sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += numbers[i];
+    }
+
+    float average = sum / size;
+
+    cout << "\nСумма: " << sum << endl;
+    cout << "Среднее арифметическое: " << average << endl;
+
+    return 0;
+}
